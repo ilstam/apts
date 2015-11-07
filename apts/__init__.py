abe9befaa7717a56e26448e6c025c1d7c3c0b2b2
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-11-07 22:30:17 +0200

    add package information

---
+++ apts/__init__.py
@@ -0,0 +1,21 @@
+"""
+Python TFTP Server
+"""
+
+# version
+__major__ = 0  # for major interface/format changes
+__minor__ = 0  # for minor interface/format changes
+__release__ = 1  # for tweaks, bug-fixes, or development
+__prerelease__ = "alpha" # alpha, beta, rc etc.
+
+# package information
+__name__ = "apts"
+__version__ = "{0}.{1}.{2}".format(__major__, __minor__, __release__)
+__version__ += __prerelease__
+__description__ = __doc__
+__author__ = "Ilias Stamatis"
+__author_email__ = "stamatis.iliass@gmail.com"
+__license__ = "GNU GPL3"
+__platforms__ = "Linux, Windows"
+__url__ = "https://github.com/Ilias95/apts"
+__download_url__ = __url__
