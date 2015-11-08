85e246723d7d579e61ddab6ebffb2cc5d05d50e7
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-11-08 17:42:14 +0200

    add config.py

---
+++ apts/config.py
@@ -0,0 +1,10 @@
+# Symbolic name, meaning all available interfaces.
+host = ''
+
+# Well-known TFTP port number
+port = 69
+
+# Maximum amount of data to be received at once.
+# Note: For best match with hardware and network realities,
+# the value of bufsize should be a relatively small power of 2.
+bufsize = 2048
