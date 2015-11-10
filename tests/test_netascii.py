a8a8c91cea05914d6697d1b71a493dace2a1e0da
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-11-10 16:22:05 +0200

    write tests for netascii encode() and decode()

--- tests/test_netascii.py
+++ tests/test_netascii.py
@@ -13,7 +13,23 @@ class TestNetascii(unittest.TestCase):
         self.assertEqual(netascii.CR, b'\x0d\x00')
 
     def test_encode(self):
-        pass
+        self.assertEqual(netascii.encode('data'.encode()), 'data'.encode())
+        self.assertEqual(netascii.encode('da\nta'.encode()),
+                                        b'da' + b'\x0d\x0a' + b'ta')
+        self.assertEqual(netascii.encode('da\rta'.encode()),
+                                        b'da' + b'\x0d\x00' + b'ta')
+        self.assertEqual(netascii.encode('da\r\nta'.encode()),
+                                        b'da' + b'\x0d\x00\x0d\x0a' + b'ta')
+        self.assertEqual(netascii.encode('da\n\rta'.encode()),
+                                        b'da' + b'\x0d\x0a\x0d\x00' + b'ta')
 
     def test_decode(self):
-        pass
+        self.assertEqual(netascii.decode('data'.encode()), 'data'.encode())
+        self.assertEqual(netascii.decode(b'da' + b'\x0d\x0a' + b'ta'),
+                                        'da\nta'.encode())
+        self.assertEqual(netascii.decode(b'da' + b'\x0d\x00' + b'ta'),
+                                         'da\rta'.encode())
+        self.assertEqual(netascii.decode(b'da' + b'\x0d\x00\x0d\x0a' + b'ta'),
+                                         'da\r\nta'.encode())
+        self.assertEqual(netascii.decode(b'da' + b'\x0d\x0a\x0d\x00' + b'ta'),
+                                         'da\n\rta'.encode())
