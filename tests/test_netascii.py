c41a17bd52c0145b79a614fd68b417b83a675642
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-11-10 18:03:42 +0200

    make netascii tests platform independent

--- tests/test_netascii.py
+++ tests/test_netascii.py
@@ -1,6 +1,11 @@
+import os
 import unittest
 
 from apts import netascii
+
+LF = netascii.LF
+CR = netascii.CR
+OSLF = os.linesep # os-specific newline
 
 
 class TestNetascii(unittest.TestCase):
@@ -9,27 +14,25 @@ class TestNetascii(unittest.TestCase):
         Netascii line feed should always be ascii CR+LF, and carriage return
         should always be ascii CR+NUL.
         """
-        self.assertEqual(netascii.LF, b'\x0d\x0a')
-        self.assertEqual(netascii.CR, b'\x0d\x00')
+        self.assertEqual(LF, b'\x0d\x0a')
+        self.assertEqual(CR, b'\x0d\x00')
 
     def test_encode(self):
         self.assertEqual(netascii.encode('data'.encode()), 'data'.encode())
-        self.assertEqual(netascii.encode('da\nta'.encode()),
-                                        b'da' + b'\x0d\x0a' + b'ta')
-        self.assertEqual(netascii.encode('da\rta'.encode()),
-                                        b'da' + b'\x0d\x00' + b'ta')
-        self.assertEqual(netascii.encode('da\r\nta'.encode()),
-                                        b'da' + b'\x0d\x00\x0d\x0a' + b'ta')
-        self.assertEqual(netascii.encode('da\n\rta'.encode()),
-                                        b'da' + b'\x0d\x0a\x0d\x00' + b'ta')
+        self.assertEqual(netascii.encode('da\rta'.encode()), b'da' + CR + b'ta')
+        self.assertEqual(netascii.encode('da{0}ta'.format(OSLF).encode()),
+                                        b'da' + LF + b'ta')
+        self.assertEqual(netascii.encode('da\r{0}ta'.format(OSLF).encode()),
+                                        b'da' + CR + LF + b'ta')
+        self.assertEqual(netascii.encode('da{0}\rta'.format(OSLF).encode()),
+                                        b'da' + LF + CR + b'ta')
 
     def test_decode(self):
         self.assertEqual(netascii.decode('data'.encode()), 'data'.encode())
-        self.assertEqual(netascii.decode(b'da' + b'\x0d\x0a' + b'ta'),
-                                        'da\nta'.encode())
-        self.assertEqual(netascii.decode(b'da' + b'\x0d\x00' + b'ta'),
-                                         'da\rta'.encode())
-        self.assertEqual(netascii.decode(b'da' + b'\x0d\x00\x0d\x0a' + b'ta'),
-                                         'da\r\nta'.encode())
-        self.assertEqual(netascii.decode(b'da' + b'\x0d\x0a\x0d\x00' + b'ta'),
-                                         'da\n\rta'.encode())
+        self.assertEqual(netascii.decode(b'da' + CR + b'ta'), 'da\rta'.encode())
+        self.assertEqual(netascii.decode(b'da' + LF + b'ta'),
+                                        'da{0}ta'.format(OSLF).encode())
+        self.assertEqual(netascii.decode(b'da' + CR + LF + b'ta'),
+                                         'da\r{0}ta'.format(OSLF).encode())
+        self.assertEqual(netascii.decode(b'da' + LF + CR + b'ta'),
+                                         'da{0}\rta'.format(OSLF).encode())
