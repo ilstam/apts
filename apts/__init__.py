2d58502da4354b7bb05cb39d3a571fba11b7ab79
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-12-22 17:42:15 +0200

    version 1.0

--- apts/__init__.py
+++ apts/__init__.py
@@ -4,16 +4,14 @@ Python TFTP Server
 
 # version
 
-__major__ = 0  # for major interface/format changes
-__minor__ = 0  # for minor interface/format changes
-__release__ = 1  # for tweaks, bug-fixes, or development
-__prerelease__ = "alpha" # alpha, beta, rc etc.
+__major__ = 1
+__minor__ = 0
+__prerelease__ = "" # alpha, beta, rc etc.
 
 # package information
 
 __name__ = "apts"
-__version__ = "{0}.{1}.{2}".format(__major__, __minor__, __release__)
-__version__ += __prerelease__
+__version__ = "{0}.{1}{2}".format(__major__, __minor__, __prerelease__)
 __description__ = __doc__
 __author__ = "Ilias Stamatis"
 __author_email__ = "stamatis.iliass@gmail.com"
